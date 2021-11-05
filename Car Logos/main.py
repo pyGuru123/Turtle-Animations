5bc9f492a27b38f7b235932e982790b776f689a6
pyguru123 <pyguru123@gmail.com>
2021-11-05 14:49:56 +0530

    added bmw logo

---
+++ Car Logos/main.py
@@ -0,0 +1,16 @@
+# 15 car logo designs
+
+import turtle
+from logos import *
+
+win = turtle.Screen()
+win.bgcolor('white')
+
+t = turtle.Turtle()
+t.width(2)
+t.hideturtle()
+
+if __name__ == '__main__':
+	bmw(t)
+	t.showturtle()
+	turtle.done()
